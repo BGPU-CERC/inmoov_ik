6a515a0c855e2876279f5a6a4a1527d2fd266677
readandwrite864 <63188034+readandwrite864@users.noreply.github.com>
2024-12-07 15:08:09 +0700

    add: module addresses

--- constants.js
+++ constants.js
@@ -4,3 +4,8 @@ export const PI = Math.PI;
 export const mapLinear = THREE.MathUtils.mapLinear;
 export const TARGET_L = 0;
 export const TARGET_R = 1;
+
+export const HEAD_ADDRESS = 0x40;
+export const LEFT_HAND_ADDRESS = 0x41;
+export const RIGHT_HAND_ADDRESS = 0x42;
+export const STOM_ADDRESS = 0x43;
